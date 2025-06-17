2c641a48992d27247dd436ab77aa3f545682e1f6
0itaps <itaps10@gmail.com>
2025-06-17 15:41:59 -0600

    feat: Complete Week 1 kinematics project
    - Implement forward kinematics for a 2-link robot using NumpPy.
    - Calculate joint positions given angles and link lengths
    -Add a Matplotlib script to visualize the robot configuration

---
+++ week_1_robotics_fundamentals/fk_calculator.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def fk_calculator(lengths, angles):
+    positions = [(0,0)]
+    x, y = 0, 0
+    total_angle = 0
+
+    for i in range(len(lengths)):
+        total_angle += angles[i]
+        x += x + lengths[i] * np.cos(total_angle)
+        y += y + lengths[i] * np.sin(total_angle)
+        positions.append((x,y))
+
+    return np.array(positions)
+
+def plot_robot(positions):
+    x_vals, y_vals = positions[:, 0], positions[:, 1]
+    plt.plot(x_vals, y_vals, 'o-')
+    plt.axis('equal')
+    plt.show()
+
+lengths = [2, 1.5, 1]
+angles = [np.pi/4, -np.pi/6, np.pi/3]
+
+positions = fk_calculator(lengths, angles)
+plot_robot(positions)
